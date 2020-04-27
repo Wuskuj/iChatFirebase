7422cfb82767f5a6e8b7e12c77c09e5c2db4366f
Philip Bratov <fil.bratov.98@mail.ru>
2020-04-27 13:45:47 +0300

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Филипп on 4/27/20.
+//  Copyright © 2020 Filipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
